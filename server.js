a4e64f3b3f0f9a069b9a37167c41684467664635
xiangdong <tianyaxiangdong@gmail.com>
2016-03-08 10:25:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -117,5 +117,5 @@ app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
+app.listen(port,"0.0.0.0");
 console.log('Magic happens on port ' + port);
