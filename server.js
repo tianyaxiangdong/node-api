38f434d7470ef8b4fb960fde83659761ff11c4cf
xiangdong <tianyaxiangdong@gmail.com>
2016-03-09 16:10:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var Bear     = require('./app/models/bear');
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
- // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
   res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
   next();
 
